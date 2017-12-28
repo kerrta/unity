05ff77e39db8370fe12c7a474c39e9ca7628a0c2
Travis Kerr <kerrta@users.noreply.github.com>
2017-12-28 17:53:47 -0500

    Player Movement Update

---
+++ Space_Shooter/Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour {
+
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		// Movement of lazer bolt
+		GetComponent<Rigidbody>().velocity = transform.forward * speed;
+	}
+	
+}
---
+++ Space_Shooter/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Boundary
+{
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public float tilt;
+
+	public Boundary boundary;
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		// LEFT and RIGHT arrow key input
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		// UP and DOWN arrow key input
+		float moveVertical = Input.GetAxis("Vertical");
+		// movement = (movement left or right based on moveHorizontal value) and (movement up or down based on moveVertical value)
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		// Adds movement to ship
+		GetComponent<Rigidbody>().velocity = movement * speed;
+
+		GetComponent<Rigidbody>().position = new Vector3(Mathf.Clamp(GetComponent<Rigidbody>().position.x, boundary.xMin, boundary.xMax), 0.0f, Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax));
+
+		GetComponent<Rigidbody>().rotation = Quaternion.Euler(0.0f, 0.0f, GetComponent<Rigidbody>().velocity.x * -tilt);
+	}
+}
